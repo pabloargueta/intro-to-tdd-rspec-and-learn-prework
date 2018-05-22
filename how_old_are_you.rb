33102ca5556ddc0100acc7bcbfc600dbc608964d
Pablo Argueta <pabloargueta27@gmail.com>
2018-05-22 01:53:14 +0000

    automatically backed up by learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,2 +1,6 @@
-require relative
-'./current_age_fpr_birth_year.rb'
+require relative './current_age_for_birth_year.rb'
+
+puts "What year were you born in?"
+birth_year = gets.to_i
+
+user_age = current_
